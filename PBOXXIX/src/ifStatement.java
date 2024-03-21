7534a007f98028f37e6f83936e23196a3437e266
peno <120492032+penoFahmi@users.noreply.github.com>
2024-03-21 15:50:22 +0700

    latihan 2

---
+++ PBOXXIX/src/ifStatement.java
@@ -0,0 +1,25 @@
+public class ifStatement {
+    public static void main(String[] args) {
+        //IF Statement
+        if (true) {
+            System.out.println("");
+        }
+
+        boolean hariIniHujan = false;
+        if (hariIniHujan) {
+            System.out.println("Saya siapkan mantel!");
+        }
+
+        byte usiaSaya = 17;
+        byte usiaDia = 19;
+        if (usiaSaya == usiaDia) {
+            System.out.println("Saya dan dia seumuran");
+        }
+
+        boolean sayaSukaDia = true;
+        boolean diaSukaSaya = true;
+        if (sayaSukaDia == diaSukaSaya)  {
+            System.out.println("Kami jadian");
+        }
+    }
+}
