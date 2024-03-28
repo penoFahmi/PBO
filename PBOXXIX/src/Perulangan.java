944a32cdbb41274b703b9013390ce86bf188a2bb
peno <120492032+penoFahmi@users.noreply.github.com>
2024-03-28 17:12:54 +0700

    latihan 3

--- PBOXXIX/src/Perulangan.java
+++ PBOXXIX/src/Perulangan.java
@@ -18,7 +18,7 @@ public class Perulangan {
             if (i % 2 == 0) {
                 continue;
             }
-            System.out.println("Loncatan ke " + i);
+            System.out.println("Hitungan ke " + i);
         }
         for (byte i = 1; i <= 20; i++) {
             if (i >=5 && i <= 15) {
@@ -26,7 +26,48 @@ public class Perulangan {
                     continue;
                 }
             }
-            System.out.println("Loncatan ke " + i);
+            System.out.println("Hitungan ke " + i);
         }
+
+        //while & dowhile
+        byte angkaWhile = 1;
+        while (angkaWhile <= 5) {
+            System.out.println("WHILE ke : " + angkaWhile);
+            angkaWhile++;
+        }
+
+        byte angkaDowWhile = 1;
+        do {
+            System.out.println("DO WHILE ke : " + angkaDowWhile);
+            angkaWhile++;
+        }while (angkaWhile <= 5);
+
+        //array
+        String[] students = {"ari","ida","tri"};
+        byte[] ages = {18, 32, 23};
+        char[] grades = {'A', 'C','B'};
+        boolean[] genders = {true, false, true};
+        int [] uangJajan = {100000, 150000, 20000};
+
+        for (int i = 0; i < students.length; i++) {
+            System.out.println(students[i]);
+        }
+        for (String student : students){
+            System.out.println("Variabel siswa kali ini valuenye : " + student);
+        }
+        for (byte age : ages) {
+            System.out.println(age);
+        }
+        for (char grade : grades) {
+            System.out.println(grade);
+        }
+        for (boolean gender : genders) {
+            System.out.println(gender);
+        }
+        for (int jajan : uangJajan ) {
+            System.out.println(jajan);
+        }
+
+
     }
 }
