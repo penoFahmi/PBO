ffce25ca55061ca791cd8ab3c352d5875d50c088
peno <120492032+penoFahmi@users.noreply.github.com>
2024-03-28 16:29:10 +0700

    pertemuan 4

--- PBOXXIX/src/Perulangan.java
+++ PBOXXIX/src/Perulangan.java
@@ -1,5 +1,32 @@
 public class Perulangan {
     public static void main(String[] args) {
-        
+
+        for (
+                byte i = 1; i <= 10; i++) {
+            System.out.println("Loncatan : " + i);
+        }
+        for (byte i = 20; i >= 10; i--) {
+            System.out.println("Loncatan ke " + i);
+        }
+        for (byte i = 22; i <= 27; i++) {
+            if (i == 24 ) {
+                continue;
+            }
+            System.out.println("Loncatan ke " + i);
+        }
+        for (byte i = 5; i <= 13; i++) {
+            if (i % 2 == 0) {
+                continue;
+            }
+            System.out.println("Loncatan ke " + i);
+        }
+        for (byte i = 1; i <= 20; i++) {
+            if (i >=5 && i <= 15) {
+                if (i % 2 == 0) {
+                    continue;
+                }
+            }
+            System.out.println("Loncatan ke " + i);
+        }
     }
 }
