ce773c34e381b91bb7dd834998de3f518c3eaf5f
peno <120492032+penoFahmi@users.noreply.github.com>
2024-04-03 19:13:46 +0700

    tugasPerulangan.

--- Tugas/src/TugasPerulangan.java
+++ Tugas/src/TugasPerulangan.java
@@ -1,44 +1,45 @@
 public class TugasPerulangan {
     public static void main(String[] args) {
         // Soal 1
-        System.out.println("Deretan angka 5, 7, 9, 13, 15:");
         for (int i = 5; i <= 15; i += 2) {
-            System.out.print(i + " ");
+            if (i == 11)continue;
+            System.out.println("Deretan ke " + i);
         }
+        System.out.println();
 
         // Soal 2
         int totalJumlah = 0;
-        System.out.println("Deretan angka 10, 9, 7, 6, 5:");
         for (int i = 10; i >= 5; i--) {
+            if (i == 8)continue;
             System.out.print(i + " ");
             totalJumlah += i;
         }
-        System.out.println("\nTotal penjumlahan: " + totalJumlah);
+        System.out.println("\nTotal Jumlah: " + totalJumlah);
+        System.out.println();
 
         // Soal 3
         int totalKali = 1;
-        System.out.println("Deretan angka 2, 4, 8, 16, 32, 64:");
         for (int i = 2; i <= 64; i *= 2) {
             System.out.print(i + " ");
             totalKali *= i;
         }
-        System.out.println("\nTotal perkalian: " + totalKali);
+        System.out.println("\nTotal Kali: " + totalKali);
+        System.out.println();
 
         // Soal 4
-        int duaPuluh = 20, tigaPuluh = 30;
+        int duaPuluh = 20;
+        int tigaPuluh = 30;
         int fpb = 1;
         for (int i = 1; i <= duaPuluh && i <= tigaPuluh; i++) {
             if (duaPuluh % i == 0 && tigaPuluh % i == 0) {
                 fpb = i;
             }
         }
-        System.out.println("Faktor Persekutuan Terbesar dari 20 dan 30: " + fpb);
+        System.out.println("Faktor Persekutuan Terbesar dari 20 dan 30 adalah " + fpb);
+        System.out.println();
 
-        // Soal 5
-        int kpt = 1;
-        while (!(kpt % duaPuluh == 0 && kpt % tigaPuluh == 0)) {
-            kpt++;
-        }
-        System.out.println("Kelipatan Persekutuan Terkecil dari 20 dan 30: " + kpt);
+        // Saol 5
+        int kpk = (duaPuluh * tigaPuluh) / fpb;
+        System.out.println("Kelipatan Persekutuan Terkecil dari 20 dan 30 adalah " + kpk);
     }
 }
